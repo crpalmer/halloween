14ad3764c1451dffdb5d2908c06ed35e542a0cea
Christopher R. Palmer <crpalmer@gmail.com>
2016-07-01 05:41:10 -0400

    jousters: Fix end-to-end version with audio
    This is just using sleeps to control the stages, I need to
    put some sensors in there instead.

--- 2016/jousters.c
+++ 2016/jousters.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "track.h"
 #include "util.h"
 #include "wb.h"
 
+#define TROT_MS 2000
+
 static char buf[1024];
 
-static int forward[2][2] = {{ -1, -1 }, { 6, 7 }};
-static int backward[2][2] = { { -1, -1 }, {5, 8 }};
+static int forward[2][2] = {{ -1, -1 }, { 6, 8 }};
+static int backward[2][2] = { { -1, -1 }, {5, 7 }};
 
 static void set(int *on, int *off, float duty)
 {
@@ -27,40 +30,63 @@ wait_for_trigger(void)
 }
 
 static void
+wait_until_start_position(void)
+{
+    ms_sleep(7500);
+}
+
+static void
 wait_until_middle(void)
 {
-	ms_sleep(3700);
+    ms_sleep(3000);
 }
 
 int
 main(int argc, char **argv)
 {
+    stop_t *stop = stop_new();
+    track_t *jousting, *crash, *beeping;
+
     if (wb_init() < 0) {
 	fprintf(stderr, "Failed to initialize wb\n");
 	exit(1);
     }
 
+    jousting = track_new_fatal("jousters_jousting.wav");
+    crash = track_new_fatal("jousters_crash.wav");
+    beeping = track_new_fatal("jousters_beeping.wav");
+
+#if 0
     set(backward[1], forward[1], 0.25);
     wait_until_start_position();
+#endif
+    set(backward[1], forward[1], 0);
 
     while (true) {
+fprintf(stderr, "Waiting\n");
 	wait_for_trigger();
-	track_play(trumpet);
+fprintf(stderr, "Starting joust\n");
+
+	track_play_asynchronously(jousting, stop);
 	set(forward[1], backward[1], 1);
-	play_asynchronously(gallop);
 	wait_until_middle();
-	cancel_async_playback();
-	track_play(crash);
-	set(forward[1], backward[1], .25);
-	play_asynchronously(trot);
+	stop_stop(stop);
+
+	track_play_asynchronously(crash, stop);
+	set(forward[1], backward[1], .35);
 	ms_sleep(TROT_MS);
 	/* gradual deceleration needed here? */
+
+	stop_stop(stop);
 	set(forward[1], backward[1], 0);
 	ms_sleep(100);
-	play_asynchronously(beep);
-	set(backward[1], forward[1], 0.35);
+
+	track_play_asynchronously(beeping, stop);
+
+	set(backward[1], forward[1], 0.5);
 	wait_until_start_position();
 	set(forward[1], backward[1], 0);
+	stop_stop(stop);
     }
 
     return 0;
