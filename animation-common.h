6baeaee8171ddc8fb996a048e4b67f466450b77b
Christopher R. Palmer <crpalmer@gmail.com>
2016-07-07 19:21:10 +0000

    animation: Swap output banks

--- animation-common.h
+++ animation-common.h
@@ -1,8 +1,8 @@
 #ifndef __ANIMATION_COMMON_H__
 #define __ANIMATION_COMMON_H__
 
-#define ANIMATION_OUTPUT_BANK   1
-#define LIGHTS_OUTPUT_BANK      2
+#define LIGHTS_OUTPUT_BANK      1
+#define ANIMATION_OUTPUT_BANK   2
 
 #define N_STATION_BUTTONS       5
 
