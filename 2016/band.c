289d5cceeb2bb327a84765fa87778318602e6953
Christopher R. Palmer <crpalmer@gmail.com>
2016-10-03 05:15:47 -0400

    2016: band: Add a mandolin prototype

--- 2016/band.c
+++ 2016/band.c
@@ -8,13 +8,22 @@
 #include "util.h"
 
 #define RECORDER_SERVO 0
+#define MANDOLIN_SERVO 2
 
+#define MANDOLIN_PCT_LOW	32
+#define MANDOLIN_PCT_HIGH	58
+#define MANDOLIN_NOTE_MS	150
+#define MANDOLIN_INTER_NOTE_LOW_MS 250
+#define MANDOLIN_INTER_NOTE_HIGH_MS 1000
+
+#define MANDOLIN_WAV	"band-mandolin.wav"
 #define RECORDER_WAV	"band-recorder.wav"
 #define SONG_WAV	"band-song.wav"
 
 static maestro_t *m;
 
 static track_t *song;
+static talking_skull_actor_t *mandolin;
 static talking_skull_actor_t *recorder;
 
 static struct timespec start;
@@ -65,6 +74,46 @@ recorder_init(void)
     recorder_down();
 }
 
+static void
+mandolin_update(void *unused, double pos)
+{
+    static int is_up = 1;
+    static struct timespec next_change = { 0 };
+    struct timespec now;
+
+    nano_gettime(&now);
+    if (pos < 5) return;
+    if (! nano_later_than(&now, &next_change)) return;
+
+    next_change = now;
+    is_up = ! is_up;
+
+    if (is_up) {
+	unsigned delta = random_number_in_range(0, MANDOLIN_INTER_NOTE_HIGH_MS - MANDOLIN_INTER_NOTE_LOW_MS);
+	unsigned ms = MANDOLIN_INTER_NOTE_LOW_MS + delta * (100 - pos) / 100;
+	nano_add_ms(&next_change, MANDOLIN_NOTE_MS + ms);
+	maestro_set_servo_pos(m, MANDOLIN_SERVO, 100);
+    } else {
+	nano_add_ms(&next_change, MANDOLIN_NOTE_MS);
+	maestro_set_servo_pos(m, MANDOLIN_SERVO, 0);
+    }
+}
+
+static void
+mandolin_init(void)
+{
+    mandolin = talking_skull_actor_new(MANDOLIN_WAV, mandolin_update, NULL);
+    if (! mandolin) {
+	perror(MANDOLIN_WAV);
+	exit(1);
+    }
+
+    maestro_set_servo_range_pct(m, MANDOLIN_SERVO, MANDOLIN_PCT_LOW, MANDOLIN_PCT_HIGH);
+    maestro_set_servo_is_inverted(m, MANDOLIN_SERVO, 1);
+    maestro_set_servo_pos(m, MANDOLIN_SERVO, 100);
+    maestro_set_servo_speed(m, MANDOLIN_SERVO, MANDOLIN_NOTE_MS);
+}
+
 int
 main(int argc, char **argv)
 {
@@ -82,10 +131,12 @@ main(int argc, char **argv)
     }
 
     recorder_init();
+    mandolin_init();
 
     while (1) {
 	nano_gettime(&start);
 	talking_skull_actor_play(recorder);
+	talking_skull_actor_play(mandolin);
 	track_play(song);
     }
 
