7cb49bbec8d5c84c93ec6e404fd8049fa053395c
Christopher R. Palmer <crpalmer@gmail.com>
2016-10-31 05:24:51 -0400

    2016: princess: Add the anvil

--- 2016/princess.c
+++ 2016/princess.c
@@ -2,10 +2,17 @@
 #include "maestro.h"
 #include "pi-usb.h"
 #include "util.h"
+#include "wb.h"
 
 #define PRINCE_SERVO		0
 #define PRINCE_HEAD_MIN		40
 #define PRINCE_HEAD_MAX		60
+
+#define ANVIL_OUTPUT		2,1
+
+#define ANVIL_DROP_MS		1000
+#define POST_SHAKE_MS		1000
+#define INTER_RUN_MS		10000
 
 static maestro_t *m;
 
@@ -22,16 +29,34 @@ shake_head(void)
     }
 }
 
+static void
+drop_anvil(void)
+{
+    wb_set(ANVIL_OUTPUT, 1);
+}
+
+static void
+retract_anvil(void)
+{
+    wb_set(ANVIL_OUTPUT, 0);
+}
+
 int main(int argc, char **argv)
 {
     pi_usb_init();
+    wb_init();
+
     m = maestro_new();
 
     maestro_set_servo_range_pct(m, PRINCE_SERVO, PRINCE_HEAD_MIN, PRINCE_HEAD_MAX);
 
     while(1) {
+	drop_anvil();
+	ms_sleep(ANVIL_DROP_MS);
 	shake_head();
-	ms_sleep(1000);
+	ms_sleep(POST_SHAKE_MS);
+	retract_anvil();
+	ms_sleep(INTER_RUN_MS);
     }
 }
 
