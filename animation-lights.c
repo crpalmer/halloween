55d750df40b6db637473c318b7f75234fffe30ef
Christopher R. Palmer <crpalmer@gmail.com>
2017-07-24 15:47:00 -0400

    Revert "animation-lights: Allow arbitrary lights, not just contiguous"
    This reverts commit 277df825e7303513e42323f578521be98d3a4b27.

--- animation-lights.c
+++ animation-lights.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <malloc.h>
-#include <memory.h>
 #include "mem.h"
 #include "util.h"
 #include "wb.h"
@@ -18,31 +17,20 @@ struct lightsS {
     pthread_mutex_t  lock;
     pthread_cond_t   cond;
     lights_action_t  action;
+    unsigned	     min_pin, max_pin;
     unsigned	     blink_pin;
-    struct {
-	int  bank;
-	int pin;
-    }		    *map;
-    int		     n;
 };
 
 
 #define ANIMATION_SLEEP_MS	200
 
 static void
-set(lights_t *l, int i, unsigned value)
-{
-fprintf(stderr, "SET %d [%d %d] =  %d\n", i, l->map[i].bank, l->map[i].pin, value);
-    wb_set(l->map[i].bank, l->map[i].pin, value);
-}
-
-static void
 set_all(lights_t *l, unsigned value)
 {
-    unsigned i;
+    unsigned pin;
 
-    for (i = 0; i < l->n; i++) {
-	set(l, i, value);
+    for (pin = l->min_pin; pin <= l->max_pin; pin++) {
+	wb_set(LIGHTS_OUTPUT_BANK, pin, value);
     }
 }
 
@@ -50,14 +38,14 @@ static void
 blink_locked(lights_t *l)
 {
     if (l->blink_pin == -1) set_all(l, 1);
-    else set(l, l->blink_pin, 1);
+    else wb_set(LIGHTS_OUTPUT_BANK, l->blink_pin, 1);
 
     pthread_mutex_unlock(&l->lock);
     ms_sleep(ANIMATION_SLEEP_MS);
     pthread_mutex_lock(&l->lock);
     if (l->action == LIGHTS_BLINK) {
 	if (l->blink_pin == -1) set_all(l, 0);
-	else set(l, l->blink_pin, 0);
+	else wb_set(LIGHTS_OUTPUT_BANK, l->blink_pin, 0);
 
 	ms_sleep(ANIMATION_SLEEP_MS);
     }
@@ -66,14 +54,14 @@ blink_locked(lights_t *l)
 static void
 chase_locked(lights_t *l)
 {
-    unsigned i;
+    unsigned pin;
 
-    for (i = 0; i < l->n && l->action == LIGHTS_CHASE; i++) {
+    for (pin = l->min_pin; pin <= l->max_pin && l->action == LIGHTS_CHASE; pin++) {
 	set_all(l, 0);
-	set(l, i, 1);
-	if (l->n >= 3) {
+	wb_set(LIGHTS_OUTPUT_BANK, pin, 1);
+	if (l->max_pin - l->min_pin + 1 >= 3) {
 	    /* If we only have 2 lights, only light up one at a time */
-	    set(l, (i + 1) % l->n, 1);
+	    wb_set(LIGHTS_OUTPUT_BANK, pin < l->max_pin ? pin+1 : l->min_pin, 1);
 	}
 	pthread_mutex_unlock(&l->lock);
 	ms_sleep(ANIMATION_SLEEP_MS);
@@ -103,28 +91,12 @@ lights_work(void *l_as_vp)
 lights_t *
 lights_new(unsigned min_pin, unsigned max_pin)
 {
-    int n = max_pin - min_pin + 1;
-    lights_map_t map[n];
-    int i;
-
-    for (i = 0; i < n; i++) {
-	map[i].bank = LIGHTS_OUTPUT_BANK;
-	map[i].pin = min_pin + 1;
-    }
-
-    return lights_new_map(map, n);
-}
-
-lights_t *
-lights_new_map(const lights_map_t *map, int n)
-{
     lights_t *l = fatal_malloc(sizeof(*l));
 
     l->action = LIGHTS_NONE;
+    l->min_pin = min_pin;
+    l->max_pin = max_pin;
     l->blink_pin = -1;
-    l->map = fatal_malloc(sizeof(*l->map) * n);
-    memcpy(l->map, map, sizeof(*l->map) * n);
-    l->n = n;
 
     pthread_mutex_init(&l->lock, NULL);
     pthread_cond_init(&l->cond, NULL);
@@ -139,7 +111,7 @@ lights_chase(lights_t *l)
 {
     pthread_mutex_lock(&l->lock);
     set_all(l, 0);
-    if (l->n <= 1) l->action = LIGHTS_BLINK;
+    if (l->min_pin == l->max_pin) l->action = LIGHTS_BLINK;
     else l->action = LIGHTS_CHASE;
     pthread_cond_signal(&l->cond);
     pthread_mutex_unlock(&l->lock);
@@ -167,7 +139,7 @@ lights_select(lights_t *l, unsigned selected)
 {
     pthread_mutex_lock(&l->lock);
     set_all(l, 0);
-    set(l, selected, 1);
+    wb_set(LIGHTS_OUTPUT_BANK, selected + l->min_pin, 1);
     l->action = LIGHTS_NONE;
     pthread_mutex_unlock(&l->lock);
 }
