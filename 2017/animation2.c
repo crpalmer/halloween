fe29c87c445dce8b4b22d9b475903d4870b9e62a
Christopher R. Palmer <crpalmer@gmail.com>
2017-10-27 11:03:32 -0400

    animation2: Program the two props

--- 2017/animation2.c
+++ 2017/animation2.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <pthread.h>
-#include "track.h"
 #include "util.h"
 #include "wb.h"
 
@@ -10,20 +9,12 @@
 #include "animation-common.h"
 #include "animation-lights.h"
 
-#define DOG  	    "dog"
-#define CAT         "cat"
-#define QUESTION    "question"
-#define TOAD        "toad"
-#define SNAKE       "snake"
+#define FROG		"frog"
+#define HOP		"hop"
 
 #define SNAKE_PIN	2, 8
 
-#define DOG_MIN_MS	5000
-#define DOG_MAX_MS	6000
-
-static pthread_mutex_t station_lock, mermaid_lock;
-static track_t *laugh;
-static stop_t *stop;
+static pthread_mutex_t station_lock;
 
 static void
 do_attack(unsigned pin, lights_t *l, double up, double down)
@@ -39,49 +30,20 @@ do_attack(unsigned pin, lights_t *l, double up, double down)
 }
 
 static void
-do_dog(void *unused, lights_t *l, unsigned pin)
+do_frog(void *unused, lights_t *l, unsigned pin)
 {
-    wb_set(ANIMATION_OUTPUT_BANK, pin, 1);
-    ms_sleep(random_number_in_range(DOG_MIN_MS, DOG_MAX_MS));
-    wb_set(ANIMATION_OUTPUT_BANK, pin, 0);
+    do_attack(pin, l, 1, 1);
 }
 
 static void
-do_cat(void *unused, lights_t *l, unsigned pin)
-{
-    do_attack(pin, l, 2, 3.0);
-}
-
-static void
-do_question(void *unused, lights_t *l, unsigned pin)
-{
-    track_play_asynchronously(laugh, stop);
-    lights_blink(l);
-    wb_set(ANIMATION_OUTPUT_BANK, pin, 1);
-    stop_await_stop(stop);
-    wb_set(ANIMATION_OUTPUT_BANK, pin, 0);
-}
-
-static void
-do_toad(void *unused, lights_t *l, unsigned pin)
+do_hop(void *unused, lights_t *l, unsigned pin)
 {
     do_attack(pin, l, 1, 1.5);
 }
 
-static void
-do_snake(void *unused, lights_t *l, unsigned pin)
-{
-    wb_set(SNAKE_PIN, 1);
-    do_attack(pin, l, 1, 3);
-    wb_set(SNAKE_PIN, 0);
-}
-
 static action_t main_actions[] = {
-    { DOG,	do_dog,		NULL },
-    { CAT,	do_cat,		NULL },		/* TODO */
-    { QUESTION, do_question,	NULL },
-    { TOAD,     do_toad,	NULL },
-    { SNAKE,	do_snake,	NULL },
+    { FROG,	do_frog,	NULL },
+    { HOP,	do_hop,		NULL },
     { NULL,	NULL,		NULL },
 };
 
@@ -94,15 +56,6 @@ int
 main(int argc, char **argv)
 {
     pthread_mutex_init(&station_lock, NULL);
-    pthread_mutex_init(&mermaid_lock, NULL);
-
-    if ((laugh = track_new("laugh.wav")) == NULL) {
-	perror("laugh.wav");
-	exit(1);
-    }
-
-    stop = stop_new();
-
     animation_main(stations);
 
     return 0;
