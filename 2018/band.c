89e4f056fe41ed7ccdcb41c337b71df2baaf1a3f
Christopher R. Palmer <crpalmer@gmail.com>
2018-10-14 12:13:01 +0000

    band: Untested attempt to add all 3 vsa controlled motions

--- 2018/band.c
+++ 2018/band.c
@@ -8,59 +8,84 @@
 #include "util.h"
 #include "wb.h"
 
-#define SINGER_SERVO	0
-#define SINGER_LEFT_EYE	1, 5
-#define SINGER_RIGHT_EYE 1, 6
-#define SINGER_SCALE	1.0
+#define EYE_BANK	1
+#define LEAD_LEFT_EYE	5
+#define LEAD_RIGHT_EYE	6
+#define BACKUP_EYES	7
+
+#define LEAD_SERVO	0
+#define BACKUP_SERVO	1
+#define BASS_SERVO	2
+#define TRIANGLE_SERVO	3
 
 #define BETWEEN_SONG_MS	1000
 
 #define SONG_WAV	"brush-test.wav"
-#define SINGER_VSA	"brush-test.csv"
+#define LEAD_VSA	"brush-lead.csv"
+#define BACKUP_VSA	"brush-backup.csv"
+#define BASS_VSA	"brush-bass.csv"
+#define TRIANGLE_VSA	"brush-triangle.csv"
 
 static maestro_t *m;
 
 static track_t *song;
-static talking_skull_actor_t *singer;
+static talking_skull_actor_t *lead, *backup, *bass;
 
 static struct timespec start;
 
+typedef struct {
+    int		servo;
+    int		eyes;
+    int		eye_pins[2];
+} servo_state_t;
+
+static servo_state_t lead_state = { LEAD_SERVO, 0, { LEAD_LEFT_EYE, LEAD_RIGHT_EYE } };
+static servo_state_t backup_state = { BACKUP_SERVO, 0, { BACKUP_EYES, -1 } };
+static servo_state_t bass_state   = { BASS_SERVO,   0, { -1, -1 } };
+
 static void
-singer_update(void *unused, double new_pos)
+servo_update(void *state_as_vp, double new_pos)
 {
-    static int eyes = -1;
+    servo_state_t *s = (servo_state_t *) state_as_vp;
     int new_eyes;
     double pos;
 
-    pos = new_pos * SINGER_SCALE;
+    pos = new_pos;
     if (pos > 100) pos = 100;
     
-    maestro_set_servo_pos(m, SINGER_SERVO, pos);
+    maestro_set_servo_pos(m, s->servo, pos);
 
     new_eyes = pos < 50;
-    if (eyes != new_eyes) {
-	wb_set(SINGER_LEFT_EYE, new_eyes);
-	wb_set(SINGER_RIGHT_EYE, new_eyes);
-	eyes = new_eyes;
+    if (s->eyes != new_eyes) {
+	if (s->eye_pins[0] >= 0) wb_set(EYE_BANK, s->eye_pins[0], new_eyes);
+	if (s->eye_pins[1] >= 0) wb_set(EYE_BANK, s->eye_pins[1], new_eyes);
+	s->eyes = new_eyes;
     }
 }
 
 static void
-singer_init(void)
+rest_servos(void)
 {
-    singer = talking_skull_actor_new_vsa(SINGER_VSA, singer_update, NULL);
-    if (! singer) {
+    servo_update(&lead_state, 100);
+    servo_update(&backup_state, 0);
+    servo_update(&bass_state, 50);
+}
+
+static void
+init_servos(void)
+{
+    lead = talking_skull_actor_new_vsa(LEAD_VSA, servo_update, &lead_state);
+    backup = talking_skull_actor_new_vsa(BACKUP_VSA, servo_update, &backup_state);
+    bass = talking_skull_actor_new_vsa(BASS_VSA, servo_update, &bass_state);
+
+    if (! lead || ! backup || ! bass) {
 	exit(1);
     }
 
-    maestro_set_servo_physical_range(m, SINGER_SERVO, 896, 1248);
-    singer_update(NULL, 100);
-}
+    maestro_set_servo_physical_range(m, LEAD_SERVO, 896, 1248);
+    /* TODO: add other ranges here */
 
-static void
-singer_rest(void)
-{
-    singer_update(NULL, 100);
+    rest_servos();
 }
 
 int
@@ -80,15 +105,17 @@ main(int argc, char **argv)
 	exit(1);
     }
 
-    singer_init();
+    init_servos();
 
     while (1) {
-	singer_rest();
+	rest_servos();
 
 	ms_sleep(BETWEEN_SONG_MS);
 
 	nano_gettime(&start);
-	talking_skull_actor_play(singer);
+	talking_skull_actor_play(lead);
+	talking_skull_actor_play(backup);
+	talking_skull_actor_play(bass);
 	track_play(song);
     }
 
