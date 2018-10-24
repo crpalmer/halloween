4f0621914ddc44c3be4a452ddd538628c6b9fe5e
Christopher R. Palmer <crpalmer@gmail.com>
2018-10-24 14:30:28 -0400

    Revert "band: Add ween-time constraints"
    This reverts commit ea07203078cad32ea239ac2563db674ce39ae629.

--- 2018/band.c
+++ 2018/band.c
@@ -7,7 +7,6 @@
 #include "track.h"
 #include "util.h"
 #include "wb.h"
-#include "ween-time.h"
 
 #define EYE_BANK	1
 #define LEAD_EYES	5
@@ -43,14 +42,6 @@ typedef struct {
 static servo_state_t lead_state = { LEAD_SERVO, 0, 0, LEAD_EYES };
 static servo_state_t backup_state = { BACKUP_SERVO, 0, 1, BACKUP_EYES };
 static servo_state_t bass_state   = { BASS_SERVO,   0, 0, -1 };
-
-static ween_time_constraint_t ween_time_constraints[] = {
-    { 0,        12, 00,         23, 00 },
-    { 5,        17, 00,         23, 00 },
-    { -1,       15, 30,         20, 00 },
-};
-
-static int n_ween_time_constraints = sizeof(ween_time_constraints) / sizeof(ween_time_constraints[0]);
 
 static void
 servo_update(void *state_as_vp, double new_pos)
@@ -121,8 +112,6 @@ main(int argc, char **argv)
 
     while (1) {
 	rest_servos();
-
-	ween_time_wait_until_valid(ween_time_constraints, n_ween_time_constraints);
 
 	ms_sleep(BETWEEN_SONG_MS);
 
--- ween-time.h
+++ ween-time.h
@@ -11,11 +11,4 @@ int ween_time_is_valid(ween_time_constraint_t *constraints, size_t n);
 
 void ween_time_wait_until_valid(ween_time_constraint_t *constraints, size_t n);
 
-static const inline int ween_time_is_halloween() {
-    ween_time_constraint_t is_halloween_constraint = { 0, 0, 0, 23, 59 };
-
-    return ween_time_is_valid(&is_halloween_constraint, 1);
-}
-
-
 #endif
