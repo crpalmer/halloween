71d5093488def0800665c29e00e6d2762186e439
Christopher R. Palmer <crpalmer@gmail.com>
2019-10-22 11:47:37 -0400

    scow: early testing

---
+++ 2019/scow.cpp
@@ -0,0 +1,110 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "externals/PIGPIO/pigpio.h"
+#include "l298n.h"
+#include "mcp23017.h"
+#include "util.h"
+#include "wb.h"
+
+static L298N *motor;
+static MCP23017 *mcp;
+static input_t *low_es, *high_es;
+
+#define ES_HIT 1
+#define UP false
+#define DOWN true
+
+static void
+test_es(const char *name, input_t *es)
+{
+    if (es->get_with_debounce() == ES_HIT) printf("  *** %s ENDSTOP ALREADY TRIGGERED!\n", name);
+    printf("Trigger %s end-stop: ", name); fflush(stdout);
+    while (es->get_with_debounce() != ES_HIT) {}
+    printf("triggered\n");
+}
+
+static void
+test_motor()
+{
+    char buf[1000];
+
+    printf("center the motor: "); fflush(stdout);
+    fgets(buf, sizeof(buf), stdin);
+    printf("motor up\n");
+    motor->direction(UP);
+    motor->speed(1);
+    ms_sleep(2*1000);
+    printf("motor down\n");
+    motor->direction(DOWN);
+    ms_sleep(2*1000);
+    motor->stop();
+}
+
+static void
+test()
+{
+    test_es("low", low_es);
+    test_es("high", high_es);
+    test_motor();
+}
+
+static void
+test_motor_es()
+{
+     printf("going up\n");
+     motor->change_motor(UP, 1);
+     ms_sleep(50);
+     while (high_es->get_with_debounce() != ES_HIT) {}
+     motor->change_motor(DOWN, 1);
+     printf("going down\n");
+     while (low_es->get_with_debounce() != ES_HIT) {}
+     motor->stop();
+}
+
+static input_t *
+get_es(int i)
+{
+    input_t *es = wb_get_input(i);
+
+    es->set_pullup_down();
+    es->set_debounce(10);
+
+    return es;
+}
+
+int
+main(int argc, char **argv)
+{
+    gpioInitialise();
+    wb_init_v2();
+
+    low_es = get_es(1);
+    high_es = get_es(2);
+    
+    mcp = new MCP23017();
+
+    motor = new L298N(mcp->get_output(0), mcp->get_output(1), mcp->get_output(2));
+    motor->stop();
+
+    motor->change_motor(UP, 1);
+char foo[100];
+    fgets(foo, sizeof(foo), stdin);
+    motor->stop();
+    exit(1);
+
+    if (argc == 2 && strcmp(argv[1], "--test") == 0) {
+	test();
+	exit(0);
+    } else if (argc == 2 && strcmp(argv[1], "--motor") == 0) {
+	test_motor();
+	exit(0);
+    } else if (argc == 2 && strcmp(argv[1], "--motor-es") == 0) {
+	test_motor_es();
+	exit(0);
+    } else if (argc > 1) {
+	printf("stopping the motor\n");
+	exit(0);
+    }
+}
+
