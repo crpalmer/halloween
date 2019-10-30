40a7bd94d63f432b1a07ba828b92860e61d5f6c2
Christopher R. Palmer <crpalmer@gmail.com>
2019-10-30 08:38:23 -0400

    2019: add fogger mainline

---
+++ 2019/fogger.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <pthread.h>
+#include "fogger.h"
+#include "util.h"
+#include "wb.h"
+
+#define FOGGER_PIN	2, 1
+
+int
+main(int argc, char **argv)
+{
+    fogger_args_t fogger_args = { 0.01, 0.01 };
+
+    if (wb_init() < 0) {
+	fprintf(stderr, "failed to initialize wb\n");
+	exit(1);
+    }
+
+    fogger_run_in_background(FOGGER_PIN, &fogger_args);
+
+    while (1) ms_sleep(1000*1000);
+
+    return 0;
+}
