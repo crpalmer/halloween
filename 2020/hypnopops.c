867700c003c369c3551a96ab20401309418ff1de
Christopher R. Palmer <crpalmer@gmail.com>
2020-10-07 16:42:23 -0400

    2020: hypnopops

---
+++ 2020/hypnopops.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "util.h"
+#include "wb.h"
+#include "ween-time.h"
+
+static ween_time_constraint_t wt[] = {
+     { 0, 9, 0, 12+10, 30 },
+     { -1, 12+3, 0, 12+6, 0 },
+     { -1, 12+7, 0, 12+8, 0 },
+};
+
+#define WT_SIZE	(sizeof(wt) / sizeof(wt[0]))
+
+int main(int argc, char **argv)
+{
+     int is_valid = -1;
+     wb_init();
+
+     while (1) {
+	int new_valid = ween_time_is_valid(wt, WT_SIZE);
+
+	if (new_valid != is_valid) {
+	    is_valid = new_valid;
+	    wb_set(1, 1, is_valid);
+	    ms_sleep(100);
+	    wb_set(2, 1, is_valid);
+	}
+	ms_sleep(1*1000);
+     }
+}
+
