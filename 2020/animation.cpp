db0f2e53f0c071492279a7fad51116b64515c0f1
Christopher R. Palmer <crpalmer@gmail.com>
2020-10-26 16:26:20 -0400

    animation: shell for 2020 props

--- 2020/animation.cpp
+++ 2020/animation.cpp
@@ -8,57 +8,88 @@
 
 class Button : public AnimationStationAction {
 public:
-    Button(output_t *light, input_t *button) {
+    Button(output_t *light, input_t *button, output_t *pin, const char *cmd = NULL) {
 	this->light = light;
 	this->button = button;
+	this->pin = pin;
+	this->cmd = cmd;
     }
 
     output_t *get_light() override { return light; }
     bool is_triggered() override { return button->get(); }
-    virtual void act(Lights *lights) { }
+    virtual void act(void) { }
+    virtual void act(Lights *lights) { act(); }
+
+    virtual char *handle_remote_cmd(const char *cmd) {
+	if (this->cmd && strcmp(cmd, this->cmd) == 0) {
+	    act();
+	    return strdup("ok");
+	}
+	return NULL;
+    }
+
+protected:
+    void attack(double up, double down) {
+	unsigned i;
+
+	for (i = 0; i < 3; i++) {
+	    pin->set(true);
+printf("up\n");
+	    ms_sleep((500 + random_number_in_range(0, 250) - 125)*up);
+	    pin->set(false);
+printf("down\n");
+	    ms_sleep((200 + random_number_in_range(0, 100) - 50)*down);
+	}
+    }
 
 private:
     output_t *light;
     input_t *button;
+    output_t *pin;
+    const char *cmd;
 };
 
-class Button1 : public Button {
+class CandyCorn : public Button {
 public:
-    Button1() : Button(wb_get_output(1, 1), wb_get_input(1)) {
+    CandyCorn() : Button(wb_get_output(1, 1), wb_get_input(1), wb_get_output(2, 1), "candy corn") {
     }
 
-    void act(Lights *lights) {
-	printf("prop 1\n");
-    }
-};
-
-class Button2 : public Button {
-public:
-    Button2() : Button(wb_get_output(1, 2), wb_get_input(2)) {
-    }
-
-    void act(Lights *lights) {
-	printf("prop 2\n");
+    void act(void) {
+	printf("candy corn\n");
+	attack(1, 1);
     }
 };
 
-class Button3 : public Button {
+class PopTots : public Button {
 public:
-    Button3() : Button(wb_get_output(1, 3), wb_get_input(3)) {
+    PopTots() : Button(wb_get_output(1, 2), wb_get_input(2), wb_get_output(2, 2), "pop tots") {
     }
 
-    void act(Lights *lights) {
-	printf("prop 3\n");
+    void act() {
+	printf("pop tots\n");
+	attack(1, 1);
     }
 };
 
-class Button4 : public Button {
+class Twizzler : public Button {
 public:
-    Button4() : Button(wb_get_output(1, 4), wb_get_input(4)) {
+    Twizzler() : Button(wb_get_output(1, 3), wb_get_input(3), wb_get_output(2, 3), "twizzler") {
     }
 
-    void act(Lights *lights) {
-	printf("prop 4\n");
+    void act() {
+	printf("twizzler\n");
+	attack(1, 1);
+    }
+};
+
+class KitKat : public Button {
+public:
+    KitKat() : Button(wb_get_output(1, 4), wb_get_input(4), wb_get_output(2, 4), "kit-kat") {
+    }
+
+    void act() {
+	printf("kit-kat\n");
+	attack(1, 1);
     }
 };
 
@@ -71,10 +102,10 @@ main(int argc, char **argv)
 
     AnimationStation *as = new AnimationStation();
     as->set_blink_ms(500);
-    as->add_action(new Button1());
-    as->add_action(new Button2());
-    as->add_action(new Button3());
-    as->add_action(new Button4());
+    as->add_action(new CandyCorn());
+    as->add_action(new PopTots());
+    as->add_action(new Twizzler());
+    as->add_action(new KitKat());
 
     AnimationStationController *asc = new AnimationStationController();
     asc->add_station(as);
