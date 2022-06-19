98003e0c0bc1df74cffb6a4bf423c74ff1222254
Christopher R. Palmer <crpalmer@gmail.com>
2022-06-19 08:16:59 -0400

    claw: trying to get a stable communication with the duet

--- 2022/claw.cpp
+++ 2022/claw.cpp
@@ -14,6 +14,7 @@
 #define BAUD	B230400
 
 static int duet;
+static char duet_reply[100000];
 
 static void
 open_duet()
@@ -30,8 +31,51 @@ open_duet()
 	exit(2);
     }
 
+#if 0
+    cfmakeraw(&tty);
+#else
+    tty.c_cflag     &=  ~PARENB;            // No Parity
+    tty.c_cflag     &=  ~CSTOPB;            // 1 Stop Bit
+    tty.c_cflag     &=  ~CSIZE;
+    tty.c_cflag     |=  CS8;                // 8 Bits
+#endif
+    tty.c_cc[VMIN]   = 1;		   // wait for atleast 1 byte
+    tty.c_cc[VTIME]  = 0;		   // blocking
+
+    tcsetattr(duet, TCSAFLUSH, &tty);
+
     cfsetispeed(&tty, BAUD);
     cfsetospeed(&tty, BAUD);
+
+    //int flags = fcntl(duet, F_GETFL, 0);
+    //fcntl(duet, F_SETFL, flags & (~O_NONBLOCK));
+
+    for (int i = 0; i < 5; i++) {
+	write(duet, "\n", 1);
+	ms_sleep(100);
+        tcflush(duet, TCIOFLUSH);
+    }
+}
+
+const char *
+duet_cmd(const char *cmd)
+{
+    int len = 0, got;
+
+    write(duet, cmd, strlen(cmd));
+    write(duet, "\n", 1);
+    while ((got = read(duet, &duet_reply[len], sizeof(duet_reply) - len)) > 0) {
+        len += got;
+ 	duet_reply[len] = '\0';
+	if ((len == 3 && strcmp(&duet_reply[len-3], "ok\n") == 0) ||
+	    (len  > 3 && strcmp(&duet_reply[len-4], "\nok\n") == 0)) {
+	    return duet_reply;
+	}
+    }
+
+duet_reply[len] = 0;
+fprintf(stderr, "partial reply: %s\n", duet_reply);
+    return NULL;
 }
 
 int main(int argc, char **argv)
@@ -43,11 +87,10 @@ int main(int argc, char **argv)
 
     open_duet();
 
-    char c;
-    write(duet, "M552\n", 5);
-    while (read(duet, &c, 1) == 1) {
-	putc(c, stdout);
-    }
+    //printf("INIT: %s\n-----\n", duet_cmd(""));
+//    printf("%s\n", duet_cmd("M122"));
+    printf("%s\n", duet_cmd("G92 X0 Y0 Z0"));
+    printf("%s\n", duet_cmd("G1 X100 F600"));
 
     return 0;
 }
