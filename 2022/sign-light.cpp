1d47154b745a3ebf8600f8b792861ab6c9fba850
Christopher R. Palmer <crpalmer@gmail.com>
2022-10-20 20:41:02 -0400

    sign-lights: A slow pulsing light for the front steps sign

---
+++ 2022/sign-light.cpp
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pico/bootrom.h>
+#include "pico/multicore.h"
+#include <tusb.h>
+#include "neopixel-pico.h"
+#include "pi.h"
+#include "time-utils.h"
+#include "util.h"
+
+#define N_LEDS 52
+#define MS	10
+#define LOW	50
+#define HIGH	255
+
+int main(int argc, char **argv)
+{
+    pi_init();
+
+    NeoPixelPico *neo = new NeoPixelPico(0);
+
+    neo->set_n_leds(N_LEDS);
+
+    for (;;) {
+	int c = HIGH;
+	while (c > LOW) {
+	    neo->set_all(c, c, c);
+	    neo->show();
+	    ms_sleep(MS);
+	    c--;
+	}
+	while (c < HIGH) {
+	    neo->set_all(c, c, c);
+	    neo->show();
+	    ms_sleep(MS);
+	    c++;
+	}
+    }
+}
