f95953f1488d769d81e70ba520602115c2b77e32
Christopher R. Palmer <crpalmer@gmail.com>
2023-07-10 06:08:33 -0400

    doug: random changes, were these the ones I used???

--- 2022/doug.cpp
+++ 2022/doug.cpp
@@ -17,10 +17,15 @@ static enum { FREQUENT, RARE } mode = RARE;
 
 #define WAIT_1 1000
 #define WAIT_2_RARE 30000
-#define WAIT_2_FREQUENT 1000
+#define WAIT_2_FREQUENT 2500
 
 static void sleep_with_button(int ms)
 {
+    if (ms > 100) {
+        printf("Sleep %5d ms\n", ms);
+	fflush(stdout);
+    }
+
     while (ms > 0) {
 	ms_sleep(ms > 20 ? 20 : ms);
 	ms -= 20;
@@ -35,12 +40,13 @@ static void sleep_with_button(int ms)
 
 void move(double l, double h, double delta)
 {
+#define MULTI_STEPS 2
     double pos = l;
 
     while ((delta < 0 ? pos >= h : pos <= h)) {
-	pos += delta;
+	pos += delta * MULTI_STEPS;
 	head->go(pos);
-	sleep_with_button(20);
+	sleep_with_button(20 * MULTI_STEPS);
     }
 }
 
@@ -49,18 +55,21 @@ main()
 {
     pi_init();
 
-    gpioInitialise();
-
     ms_sleep(1000);
+
+    gpioInitialise();
 
     head = new Servo(0);
     button = new GPInput(1);
     button->set_pullup_up();
 
     while (1) {
+	printf("Go to HIGH\n");
 	move(LOW, HIGH, +.002);
 	sleep_with_button(WAIT_1);
+	printf("Go to LOW\n");
 	move(HIGH, LOW, -.008);
-	sleep_with_button(mode == RARE ? WAIT_2_RARE : WAIT_2_FREQUENT);
+	int ms = (mode == RARE ? WAIT_2_RARE : WAIT_2_FREQUENT);
+	sleep_with_button(ms);
     }
 }
