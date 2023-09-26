eb2de99b0910d0665e1aca0daf9dc232362a5af3
Christopher R. Palmer <crpalmer@gmail.com>
2023-09-26 14:57:42 -0400

    cheshire: Randomly fade parts in and out

--- 2023/cheshire.cpp
+++ 2023/cheshire.cpp
@@ -8,11 +8,20 @@
 #include "time-utils.h"
 #include "util.h"
 
+#define N_NEO		4
+
+#define ALL_P		0.05
 #define FADE_INC	1
 #define FADE_SLEEP_MS	10
 
+static NeoPixelPico *neo[N_NEO];
+static int n_leds[N_NEO] = { 14, 13, 8, 47 };
+static bool lit[N_NEO] = { 0, };
+
+const char *names[] = { "left-eye", "right-eye", "nose", "mouth" };
+
 static void
-fade_in(NeoPixelPico **neo, int n_neo, int final_r, int final_g, int final_b)
+fade_in(int *set, int n_set, int final_r, int final_g, int final_b)
 {
     int r = 0, g = 0, b = 0;
 
@@ -20,64 +29,141 @@ fade_in(NeoPixelPico **neo, int n_neo, int final_r, int final_g, int final_b)
 	if (r < final_r) r += FADE_INC;
 	if (g < final_g) g += FADE_INC;
 	if (b < final_b) b += FADE_INC;
-	for (int i = 0; i < n_neo; i++) {
-	    neo[i]->set_all(r, g, b);
-	    neo[i]->show();
+	for (int i = 0; i < n_set; i++) {
+	    neo[set[i]]->set_all(r, g, b);
+	    neo[set[i]]->show();
 	}
 	ms_sleep(FADE_SLEEP_MS);
     }
-    printf("faded in to %d,%d,%d\n", r, g, b);
 }
 
 static void
-fade_out(NeoPixelPico **neo, int n_neo, int r, int g, int b)
+fade_out(int *set, int n_set, int r, int g, int b)
 {
     while (r > 0 || g > 0 || b > 0) {
 	if (r > 0) r -= FADE_INC;
 	if (g > 0) g -= FADE_INC;
 	if (b > 0) b -= FADE_INC;
-	for (int i = 0; i < n_neo; i++) {
-	    neo[i]->set_all(r, g, b);
-	    neo[i]->show();
+	for (int i = 0; i < n_set; i++) {
+	    neo[set[i]]->set_all(r, g, b);
+	    neo[set[i]]->show();
 	}
 	ms_sleep(FADE_SLEEP_MS);
     }
-    printf("fade out complete.\n");
+}
+
+static int
+select_all(int *set)
+{
+    int n = 0;
+
+    for (int i = 0; i < N_NEO; i++) {
+	if (! lit[i]) {
+	    set[n] = i;
+	    lit[n] = true;
+	    n++;
+	}
+    }
+    return n;
+}
+
+static int
+select_set(int *set, bool desired_state, int max_n)
+{
+    int n = 0;
+
+    for (int i = 0; i < N_NEO; i++) {
+	if (lit[i] == desired_state) {
+	    set[n++] = i;
+	}
+    }
+
+    if (n) {
+	int n_needed = random_number_in_range(1, max_n-1);
+	while (n > n_needed) {
+	    int reject = random_number_in_range(0, n-1);
+	    set[reject] = set[n-1];
+	    n--;
+	}
+    }
+
+    for (int i = 0; i < n; i++) {
+	lit[set[i]] = ! lit[set[i]];
+    }
+
+    return n;
+}
+
+static void
+dump_set(const char *msg, int *set, int n_set)
+{
+    printf("%s:", msg);
+    for (int i = 0; i < n_set; i++) {
+	printf(" %s", names[set[i]]);
+    }
+    printf("\n");
+}
+
+static void
+dump_state()
+{
+    printf("lit:");
+    for (int i = 0; i < N_NEO; i++) {
+	if (lit[i]) printf(" %s", names[i]);
+    }
+    printf("\n");
 }
 
 int main(int argc, char **argv)
 {
-    NeoPixelPico *neo[4];
+    double all_P = ALL_P;
+
+    seed_random();
 
     pi_init_no_reboot();
 
-    neo[0] = new NeoPixelPico(0);
-    neo[0]->set_n_leds(14);
-    neo[1] = new NeoPixelPico(1);
-    neo[1]->set_n_leds(13);
-    neo[2] = new NeoPixelPico(2);
-    neo[2]->set_n_leds(8);
-    neo[3] = new NeoPixelPico(3);
-    neo[3]->set_n_leds(47);
+    for (int i = 0; i < N_NEO; i++) {
+        neo[i] = new NeoPixelPico(i, true);
+        neo[i]->set_n_leds(n_leds[i]);
+	neo[i]->set_all(255, 0, 0);
+	neo[i]->show();
+	lit[i] = true;
+    }
 
-    NeoPixelPico *eyes[2] = { neo[0], neo[1] };
-    NeoPixelPico *nose = neo[2];
-    NeoPixelPico *eyes_nose[3] = { eyes[0], eyes[1], nose };
-    NeoPixelPico *mouth = neo[3];
-
-    const char *names[] = { "left eye", "right eye", "nose", "mouth" };
+    printf("Sleeping before starting\n");
+    ms_sleep(2000);
+    printf("Starting\n");
 
     for (;;) {
-	fade_in(neo, 4, 255, 0, 0);
-	ms_sleep(1000);
-	fade_out(eyes_nose, 3, 255, 0, 0);
-        ms_sleep(500);
-	fade_out(&mouth, 1, 255, 0, 0);
-	ms_sleep(1000);
+	int set[N_NEO];
+	int n_set;
 
-	fade_in(neo, 4, 255, 0, 0);
-	ms_sleep(1000);
-	fade_out(neo, 4, 255, 0, 0);
-	ms_sleep(1000);
+	if (random_number() < all_P) {
+	    n_set = select_all(set);
+	    dump_set("all", set, n_set);
+	    all_P = ALL_P;
+	} else {
+	    all_P += ALL_P;
+	    n_set = select_set(set, false, N_NEO);
+	    dump_set(" in", set, n_set);
+        }
+	if (n_set) {
+	    fade_in(set, n_set, 255, 0, 0);
+	    dump_state();
+
+	    ms_sleep(1000);
+	}
+
+	n_set = select_set(set, true, N_NEO);
+	if (n_set) {
+	    dump_set("out", set, n_set);
+	    fade_out(set, n_set, 255, 0, 0);
+
+	    dump_state();
+
+	    ms_sleep(1000);
+	}
+
+	printf("\n");
     }
 }
