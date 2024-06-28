8647023e3e29569f2a92a030dd06b0cf957d51c6
Christopher R. Palmer <crpalmer@gmail.com>
2024-06-28 07:05:14 -0400

    fart-blaster: make light actions into state machines
    This lets me run the main chamber and the bubble independently
    with a single thread.  The advantage of doing so is that I don't
    have to either have 2 NeoPixelPico objects or synchronize between
    2 threads sharing a single object.
    
    Note: I had to restrict the total number of lights lit by the
    purlsing because the power draw is higher than the pi5 USB port
    can provide.

--- 2024/fart-blaster.cpp
+++ 2024/fart-blaster.cpp
@@ -3,82 +3,154 @@
 #include "pi-threads.h"
 #include "time-utils.h"
 
-static const int n_leds = 7;
 static const int neo_gpio = 28;
-static NeoPixelPico *neo;
+static const int n_main_leds = 35;
+static const int n_bubble_leds = 7;
+static const int total_n_leds = n_main_leds + n_bubble_leds;
 
 #define FADE_INC	2
 #define FADE_SLEEP_MS	7
-#define RING_SLEEP_MS	20
+#define VORTEX_ROTATION_MS 100
 #define ON_VALUE	254
 #define OFF_VALUE	50
 
-static void ring_one(int i) {
-    neo->set_all(0, 0, 0);
-    neo->set_led(0, ON_VALUE, ON_VALUE, ON_VALUE);
-    neo->set_led(i, ON_VALUE, ON_VALUE, ON_VALUE);
-    neo->show();
-}
-
-static void ring() {
-    for (int i = 1; i < n_leds; i++) {
-	ring_one(i);
-	ms_sleep(RING_SLEEP_MS);
+class LightAction {
+public:
+    LightAction(NeoPixelPico *neo, int first_led, int n_leds) : neo(neo), first_led(first_led), n_leds(n_leds) {
+	nano_gettime(&next);
     }
-}
 
-static void speed_up_ring() {
-    for (int it = 0; it < (RING_SLEEP_MS-5)*3; it++) {
-	for (int i = 1; i < n_leds; i++) {
-	    ring_one(i);
-	    ms_sleep(RING_SLEEP_MS - it/3);
+    virtual void step() = 0;
+
+    bool is_ready() {
+	return nano_now_is_later_than(&next);
+    }
+
+    void schedule_in(int ms) {
+	nano_gettime(&next);
+	nano_add_ms(&next, ms);
+    }
+
+protected:
+    void set_all(int r, int g, int b) {
+	for (int i = 0; i < n_leds; i++) set_led(i, r, g, b);
+    }
+
+    void set_all(int colour) { set_all(colour, colour, colour); }
+
+    void set_led(int led, int r, int g, int b) {
+	neo->set_led(led + first_led, r, g, b);
+    }
+
+    void set_led(int led, int colour) { set_led(led, colour, colour, colour); }
+
+    int get_n_leds() { return n_leds; }
+
+private:
+    NeoPixelPico *neo;
+    int first_led;
+    int n_leds;
+
+    struct timespec next;
+};
+
+class LightVortex : public LightAction {
+public:
+    LightVortex(NeoPixelPico *neo, int first_led, int n_leds, int n_active = 1) : LightAction(neo, first_led, n_leds), n_active(n_active) {
+    }
+
+    void step() override {
+	int n_leds = get_n_leds();
+
+	set_all(0);
+	set_led(0, ON_VALUE);
+	for (int i = it; i < it + n_active; i++) {
+	    set_led(i % n_leds, ON_VALUE);
 	}
+	schedule_in(VORTEX_ROTATION_MS / n_leds);	// To do accelerate
+	if (++it >= n_leds) it = 1;
     }
-}
 
-static void fade_in() {
-    int r = OFF_VALUE, g = OFF_VALUE, b = OFF_VALUE;
+private:
+    int it = 1;
+    int n_active;
+};
 
-    while (r < ON_VALUE || g < ON_VALUE || b < ON_VALUE) {
-	if (r < ON_VALUE) r += FADE_INC;
-	if (g < ON_VALUE) g += FADE_INC;
-	if (b < ON_VALUE) b += FADE_INC;
-	neo->set_all(r, g, b);
-	neo->show();
-	ms_sleep(FADE_SLEEP_MS);
+class PulsingLight : public LightAction {
+public:
+    PulsingLight(NeoPixelPico *neo, int first_led, int n_leds, int n_groups = 1) : LightAction(neo, first_led, n_leds), n_groups(n_groups) {
     }
-}
 
-static void fade_out() {
-    int r = ON_VALUE, g = ON_VALUE, b = ON_VALUE;
+    void step() override {
+	set_all(0);
+	for (int i = cur_group; i < get_n_leds(); i += n_groups) {
+	    set_led(i, colour);
+	}
+	//cur_group = (cur_group + 1) % n_groups;
 
-    while (r > OFF_VALUE || g > OFF_VALUE || b > OFF_VALUE) {
-	if (r > OFF_VALUE) r -= FADE_INC;
-	if (g > OFF_VALUE) g -= FADE_INC;
-	if (b > OFF_VALUE) b -= FADE_INC;
-	neo->set_all(r, g, b);
-	neo->show();
-	ms_sleep(FADE_SLEEP_MS);
+	if (is_entering) {
+	    colour += FADE_INC;
+	    if (colour > ON_VALUE) {
+		colour = ON_VALUE;
+		is_entering = false;
+	    }
+	} else {
+	    colour -= FADE_INC;
+	    if (colour < OFF_VALUE) {
+		colour = OFF_VALUE;
+		is_entering = true;
+	    }
+	}
+	schedule_in(FADE_SLEEP_MS);
     }
-}
 
+private:
+    bool is_entering = true;
+    int colour = OFF_VALUE;
+    int n_groups;
+    int cur_group = 0;
+};
+
+#define A 0
 static void threads_main(int argc, char **argv) {
-    neo = new NeoPixelPico(neo_gpio);
-    neo->set_n_leds(n_leds);
+    NeoPixelPico *neo = new NeoPixelPico(neo_gpio);
+    neo->set_n_leds(total_n_leds);
 
-    neo->set_all(0, 0, 0);
-    neo->show();
+    LightAction *bubble_vortex = new LightVortex(neo, n_main_leds, n_bubble_leds);
+    LightAction *bubble_pulse = new PulsingLight(neo, n_main_leds, n_bubble_leds);
+    LightAction *main_vortex = new LightVortex(neo, 0, n_main_leds, 5);
+    LightAction *main_pulse = new PulsingLight(neo, 0, n_main_leds, 4);
 
-    while (true) {
-	for (int i = 0; i < 5; i++) {
-	    speed_up_ring();
+    int steps = 0;
+    LightAction *bubble = bubble_pulse;
+    LightAction *main = main_vortex;
+
+    while (1) {
+	bool dirty = false;
+
+	if (bubble->is_ready()) {
+	    bubble->step();
+	    dirty = true;
 	}
-	for (int i = 0; i < 50; i++) {
-	    ring();
+
+	if (main->is_ready()) {
+	    main->step();
+	    dirty = true;
 	}
-	for (int i = 0; i < 10; i++) {
-	    fade_in();
- 	    fade_out();
+
+	if (dirty) neo->show();
+
+	ms_sleep(1);
+	steps++;
+
+	if (steps % 5000 == 0) {
+	    if (bubble == bubble_pulse) bubble = bubble_vortex;
+	    else bubble = bubble_pulse;
+	}
+
+	if (steps % 3456 == 0) {
+	    if (main == main_pulse) main = main_vortex;
+	    else main = main_pulse;
 	}
     }
 }
