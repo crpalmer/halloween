a2d137c8ff89901f4bfc2e114751e0d3c73a571a
Christopher R. Palmer <crpalmer@gmail.com>
2025-10-18 11:19:31 -0400

    mushrooms: working

--- 2025/mushrooms.cpp
+++ 2025/mushrooms.cpp
@@ -3,23 +3,37 @@
 #include "gp-output.h"
 #include "stepper.h"
 
-const int DIR_GPIO = 0;
-const int STEP_GPIO = 1;
+const int BASE_PIN[] = { 0, 2 };
 const double STEPS_FOR_FULL_ROTATION = 360/1.8;
-const double MM_PER_FULL_ROTATION = 80;
-const double STEPS_PER_MM = MM_PER_FULL_ROTATION / STEPS_FOR_FULL_ROTATION;
+const double MM_PER_FULL_ROTATION = 120;
+const double STEPS_PER_MM = STEPS_FOR_FULL_ROTATION / MM_PER_FULL_ROTATION;
+const double DISTANCE_MM = 1900;
+const double TIME_SEC = 3;
+
+class Mushroom : public PiThread {
+public:
+    Mushroom(int base_pin) : PiThread("mushroom") {
+	stepper = new Stepper(base_pin, STEPS_PER_MM);
+	start();
+    }
+
+    void main(void) {
+	while (1) {
+	    stepper->go(DISTANCE_MM-1, DISTANCE_MM/TIME_SEC);
+	    stepper->go(1, DISTANCE_MM/TIME_SEC);
+	}
+    }
+
+private:
+    Stepper *stepper;
+};
 
 void thread_main(int argc, char **argv) {
     ms_sleep(1000);
     printf("Starting\n");
-    printf("dir %d step %d steps/mm %f\n", DIR_GPIO, STEP_GPIO, STEPS_PER_MM);
-    Stepper *stepper = new Stepper(new GPOutput(DIR_GPIO), new GPOutput(STEP_GPIO), STEPS_PER_MM);
-    while (1) {
-	printf("high\n");
-	stepper->go(72*254);
-	printf("low\n");
-	stepper->go(0);
-    }
+    printf("dir %d step %d steps/mm %f\n", BASE_PIN[0], BASE_PIN[1], STEPS_PER_MM);
+    new Mushroom(BASE_PIN[0]);
+    new Mushroom(BASE_PIN[1]);
 }
 
 int main(int argc, char **argv) {
