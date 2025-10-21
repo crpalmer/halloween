2b2c2967573b43f34db7b069a95db3dbb29988cb
Christopher R. Palmer <crpalmer@gmail.com>
2025-10-21 08:16:27 -0400

    mushrooms: add an interactive test mode

--- 2025/mushrooms.cpp
+++ 2025/mushrooms.cpp
@@ -1,16 +1,22 @@
 #include "pi.h"
 #include "gp-input.h"
 #include "gp-output.h"
+#include "pi-threads.h"
+#include "stdin-reader.h"
+#include "stdout-writer.h"
 #include "stepper.h"
+#include "threads-console.h"
 
 static const int STEPPER_PIN0 = 0;
 static const int ES_PIN = 2;
 
-const double STEPS_FOR_FULL_ROTATION = 360/1.8;
-const double MM_PER_FULL_ROTATION = 120;
-const double STEPS_PER_MM = STEPS_FOR_FULL_ROTATION / MM_PER_FULL_ROTATION;
-const double DISTANCE_MM = 1900;
-const double TIME_SEC = 3;
+static const double STEPS_FOR_FULL_ROTATION = 360/1.8;
+static const double MM_PER_FULL_ROTATION = 120;
+static const double STEPS_PER_MM = STEPS_FOR_FULL_ROTATION / MM_PER_FULL_ROTATION;
+static const double DISTANCE_MM = 1900;
+static const double TIME_SEC = 3;
+
+static const bool TEST_MODE = true;
 
 class Mushroom : public PiThread {
 public:
@@ -30,10 +36,53 @@ private:
     Stepper *stepper;
 };
 
+class ConsoleThread : public ThreadsConsole, public PiThread {
+public:
+    ConsoleThread() : ThreadsConsole(new StdinReader(), new StdoutWriter()) {
+	stepper = new Stepper(STEPPER_PIN0, STEPS_PER_MM);
+	endstop = new GPInput(ES_PIN);
+	endstop->set_pullup_up();
+	start();
+    }
+
+    void main() {
+	Console::main();
+    }
+
+    void process_cmd(const char *cmd) override {
+	const char *p = strchr(cmd, ' ');
+	double arg;
+	bool have_arg = (p && sscanf(p, "%lg", &arg) == 1);
+
+        if (have_arg && is_command(cmd, "move-to")) {
+	    stepper->go(arg, mm_sec);
+	} else if (have_arg && is_command(cmd, "set-speed")) {
+	    mm_sec = arg;
+	} else if (is_command(cmd, "status")) {
+	    printf("pos %g speed %g endstop %s\n", pos, mm_sec, endstop->get() ? "triggered" : "not");
+        } else {
+            ThreadsConsole::process_cmd(cmd);
+        }
+    }
+
+    void usage() override {
+        ThreadsConsole::usage();
+	printf("move-to <mm>\nset-speed <mm/sec>\nstatus\n");
+    }
+
+private:
+    Stepper *stepper = NULL;
+    Input *endstop = NULL;
+
+    double mm_sec = 200;
+    double pos = 0;
+};
+
 void thread_main(int argc, char **argv) {
     ms_sleep(1000);
     printf("Starting\n");
-    new Mushroom(STEPPER_PIN0);
+    if (TEST_MODE) new ConsoleThread();
+    else new Mushroom(STEPPER_PIN0);
 }
 
 int main(int argc, char **argv) {
